e843a763a8b9123e9408dfc89523cf7a9dc33958
tazzo <roberto.tazzoli@gmail.com>
2017-05-09 00:14:34 +0200

    added port communication

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,12 +1,76 @@
-{- This file re-implements the Elm Counter example (1 counter) with elm-mdl
-   buttons. Use this as a starting point for using elm-mdl components in your own
-   app.
--}
-
-
-module Main exposing (..)
+port module Main exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (..)
 
 
-main = text "ciao"
+type alias Model =
+    { user : String
+    }
+
+
+type Msg
+    = Msg String
+    | Firebase String
+
+
+initModel : Model
+initModel =
+    { user = "out" }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick <| Msg "ciao" ] [ text "button in" ]
+        , button [] [ text "button out" ]
+        , text model.user
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Msg str ->
+            let
+                a =
+                    Debug.log str 1
+            in
+                ( model, firebaseSend str )
+
+        Firebase str ->
+            let
+                a =
+                    Debug.log str 2
+            in
+                ( model, Cmd.none )
+
+
+
+-- port for sending strings out to JavaScript
+
+
+port firebaseSend : String -> Cmd msg
+
+
+
+-- port for listening for suggestions from JavaScript
+
+
+port firebaseReceive : (String -> msg) -> Sub msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ firebaseReceive Firebase ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = ( initModel, Cmd.none )
+        , view = view
+        , subscriptions = subscriptions
+        , update = update
+        }
