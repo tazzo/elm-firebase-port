10ea15645e2f10f230ca65ffb8efd0a95b7a970a
tazzo <roberto.tazzoli@gmail.com>
2017-05-08 14:39:44 +0200

    initial commit

---
+++ src/elm/Main.elm
@@ -0,0 +1,12 @@
+{- This file re-implements the Elm Counter example (1 counter) with elm-mdl
+   buttons. Use this as a starting point for using elm-mdl components in your own
+   app.
+-}
+
+
+module Main exposing (..)
+
+import Html exposing (..)
+
+
+main = text "ciao"
